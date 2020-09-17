67008a9272e29696d874fc60d82f97aef1deea8d
yundowon <70631088+yundowon@users.noreply.github.com>
2020-09-17 16:28:19 +0900

    exercise03

--- main.c
+++ main.c
@@ -5,13 +5,11 @@
 
 int main(int argc, char *argv[]) {
 	char c;
-	int i;
 	
-	printf("input a number:");
+	printf("enter a character:");
 	scanf("%c", &c);
 	
-	i= c - '0';
-	printf("The input number is %i\n", i);
+	printf("the next character of %c (%d) is %c (%d)\n", c, c, c+1, c+1);
 	
 	return 0;
 }
